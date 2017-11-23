5de08384919f7f0c3f30557e1b9dbdfbaa5b331c
Alon1990 <alonbarkan1@gmail.com>
2017-11-23 17:38:18 +0200

    after updating test_DefaultLogic.cpp (add tests to CheckPossibleMoves and IsGameOver)

--- test/ReversiTests/test_DefaultLogic.cpp
+++ test/ReversiTests/test_DefaultLogic.cpp
@@ -10,20 +10,68 @@
 
 #include "gtest/gtest.h"
 #include "../../include/DefaultLogic.h"
+#include <fstream>
+
+
+
+const Board createBoardFromFile(const char* fileName) {
+
+    string line;
+    string boardStr;
+    ifstream myfile(fileName);
+
+    if (myfile.is_open()) {
+        for (int i = 0; i < NUM_OF_ROWS; i++) {
+            getline(myfile, line);
+            boardStr += line;
+            boardStr += "\n";
+        }
+    }
+    myfile.close();
+
+
+    Board b = Board(boardStr);
+    return b;
+}
 
 
 TEST(CheckPossibleMovesMeothd, CheckMovesVecDataMember) {
 
     DefaultLogic dl = DefaultLogic();
-    Board* b = new Board();
 
-    dl.CheckPossibleMoves(b,xplayer);
+    Board b = Board();
+
+    dl.CheckPossibleMoves(&b,xplayer);
     EXPECT_EQ(dl.GetMoves().size(),4);
 
-    dl.CheckPossibleMoves(b,yplayer);
+    dl.CheckPossibleMoves(&b,yplayer);
     EXPECT_EQ(dl.GetMoves().size(),4);
 
- //   while ()
+
+
+
+
+    Board b1 = createBoardFromFile("noPossibleMovesForX.txt");
+    dl.CheckPossibleMoves(&b1, xplayer);
+    EXPECT_EQ(dl.GetMoves().size(), 0);
+
+
+
+
+
+
+    Board b2 = createBoardFromFile("eightPossMovesForO.txt");
+    dl.CheckPossibleMoves(&b2, yplayer);
+    EXPECT_EQ(dl.GetMoves().size(), 8);
+
+
+
+
+    Board b3 = createBoardFromFile("noPossibleMovesForO.txt");
+    dl.CheckPossibleMoves(&b3, yplayer);
+    EXPECT_EQ(dl.GetMoves().size(), 0);
+
+
 
 
 
@@ -32,21 +80,56 @@ TEST(CheckPossibleMovesMeothd, CheckMovesVecDataMember) {
 TEST (IsGameOverMethod, CheckIfGameIsOver) {
 
     DefaultLogic dl = DefaultLogic();
-    Board* b = new Board();
+    Board b = Board();
 
     for (int i = 0; i < NUM_OF_ROWS; i++) {
         for (int j = 0; j < NUM_OF_COLS; j++) {
-            b->getBoardContent()[i][j] = X;
+            b.getBoardContent()[i][j] = X;
         }
     }
-    EXPECT_TRUE(dl.IsGameOver(b));
+    EXPECT_TRUE(dl.IsGameOver(&b));
 
     for (int i = 0; i < NUM_OF_ROWS; i++) {
         for (int j = 0; j < NUM_OF_COLS; j++) {
-            b->getBoardContent()[i][j] = O;
+            b.getBoardContent()[i][j] = O;
         }
     }
-    EXPECT_TRUE(dl.IsGameOver(b));
+
+    EXPECT_TRUE(dl.IsGameOver(&b));
+
+
+
+    Board b1 = createBoardFromFile("noPossibleMovesForX.txt");
+    EXPECT_FALSE(dl.IsGameOver(&b1));
+
+
+
+    Board b2 = createBoardFromFile("noPossibleMovesForO.txt");
+    EXPECT_FALSE(dl.IsGameOver(&b2));
+
+
+
+    //add test to case where board isnt full, and both players have no possible moves
+
+
+ //   dl.CheckPossibleMoves(&b1, xplayer);
+
+ //   EXPECT_EQ(dl.GetMoves().size(), 0);
+
+
+    //   EXPECT_TRUE();
+
+
+
+
+
+
+
+
+
+
+
+
 }
 
 /*
